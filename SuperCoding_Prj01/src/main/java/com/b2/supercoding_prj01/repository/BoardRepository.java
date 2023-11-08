29fbf06cd228b56b002f3095b47f4577e66f4780
leekkkrrr <gyoungri21@gmail.com>
2023-11-08 22:17:41 +0900

    Feat: Repository 수정
    CommentsRepository에 findAll/findByBoard 메서드 추가.

--- SuperCoding_Prj01/src/main/java/com/b2/supercoding_prj01/repository/BoardRepository.java
+++ SuperCoding_Prj01/src/main/java/com/b2/supercoding_prj01/repository/BoardRepository.java
@@ -4,8 +4,14 @@ import com.b2.supercoding_prj01.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
 
+    Optional<BoardEntity> findByBoardId(Long boardId);
+
+    List<BoardEntity> findByAuthor(String Author);
 }
